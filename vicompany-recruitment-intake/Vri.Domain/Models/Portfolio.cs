c60d0fd7bdc90af56c8874dd2a0a33670604f0c0
eomar <25548321+Eomar21@users.noreply.github.com>
2025-04-08 11:13:46 +0200

    initial commit

---
+++ vicompany-recruitment-intake/Vri.Domain/Models/Portfolio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Vri.Domain.Models;
+
+public class Portfolio
+{
+    public Portfolio(decimal startCash, IEnumerable<Transaction> transactions)
+    {
+        // TODO: implement
+    }
+
+    public decimal CashPosition { get; set; }
+
+    public IReadOnlyList<Position> Instruments { get; set; } = new List<Position>(); 
+}
